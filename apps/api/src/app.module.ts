1bcb33d674a21b211d38268164f3856b08f120d3
lucas-laza <laza.lucas15@gmail.com>
2023-12-20 09:54:08 +0100

    feat(db): connexion mongo + user dans dto

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { CreateParkSlot } from './services/ParkSlot/CreateParkSlot.service';
+import { AppController } from './infrastructure/controller/app.controller';
+// import { CreateParkSlot } from './domain/services/CreateParkSlot.service';
+import { JwtModule } from '@nestjs/jwt';
+
 
 import { MongooseModule } from '@nestjs/mongoose';
-import { ParkModule } from './modules/park.module';
+import { ParkModule } from './infrastructure/repository/park.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/coucou'), ParkModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/coucou'),
+    ParkModule,
+    JwtModule.register({
+      secret: '!park59zen800coucou*', // Change this to a secure secret key
+      signOptions: { expiresIn: '1h' }, // Set the expiration time for the token
+    })
+  ],
   controllers: [AppController],
-  providers: [CreateParkSlot],
+  providers: [],
 })
 export class AppModule {}
--- apps/api/src/domain/entities/Park.entity.ts
+++ apps/api/src/domain/entities/Park.entity.ts
@@ -1,9 +1,11 @@
+// park-slot.entity.ts
 import { CreateParkDTO } from 'src/dto/CreatePark.dto';
 import { Address } from './Address.entity';
 import { Caracteristics } from './Caracteristics.entity';
 import { Renter } from './Renter.entity';
 import { SpaceType } from './SpaceType.entity';
 import { randomUUID } from 'crypto';
+import { RenterService } from '../services/Renter.service';
 
 export class ParkSlot {
   id: string;
@@ -15,22 +17,39 @@ export class ParkSlot {
   spaceType: SpaceType;
   caracteristics: Caracteristics;
   createdAt: Date;
+  private renterService: RenterService;
 
-  private constructor(data: CreateParkDTO) {
+  private constructor(data: CreateParkDTO, renterService: RenterService) {
     const id = randomUUID();
     const createdAt = new Date();
 
     this.id = id;
     this.address = new Address(data.address);
     this.priceForOneHour = data.priceForOneHour;
-    this.renter = new Renter(data.renter);
+    // this.renter = new Renter(data.renter);
+    this.renter = data.renter;
     this.rentStartAt = data.rentStartAt;
     this.rentEndAt = data.rentEndAt;
     this.spaceType = new SpaceType(data.spaceType);
     this.caracteristics = new Caracteristics(data.caracteristics);
     this.createdAt = createdAt;
+    this.renterService = renterService;
+    console.log(data);
+    
+    // if (data.user_id) {
+    //   this.fetchRenter(data.user_id).then((renter) => {
+    //     this.renter = renter;
+    //     console.log(renter);
+    //   });
+    // }
   }
-  static createByUser(data: CreateParkDTO) {
-    return new ParkSlot(data);
+
+  async fetchRenter(userId: number): Promise<Renter> {
+    const renter = await this.renterService.findOne(userId);
+    return new Renter(renter);
+  }
+
+  static createByUser(data: CreateParkDTO, renterService: RenterService) {
+    return new ParkSlot(data, renterService);
   }
 }
