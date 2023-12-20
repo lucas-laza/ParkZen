9d1f3e9a02db42ee1be9aa11f65dfca3e20da99a
Your Name <you@example.com>
2023-12-20 11:59:55 +0100

    feat(api): ajout champs base

--- apps/api/src/infrastructure/repository/schemas/park.schema.ts
+++ apps/api/src/infrastructure/repository/schemas/park.schema.ts
@@ -2,7 +2,9 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { HydratedDocument } from 'mongoose';
 import { Address } from './address.schema';
-
+import { Renter } from './renter.schema';
+import { Caracteristics } from './caracteristics.schema';
+import { SpaceType } from './spaceType.schema';
 export type ParkDocument = HydratedDocument<Park>;
 
 @Schema()
@@ -14,11 +16,18 @@ export class Park {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Address',
   })
+  @Prop({
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Renter',
+  })
   address: Address;
   @Prop({ required: true })
   priceForOneHour: number;
-  // @Prop({ required: true })
-  // renter: object;
+  renter: Renter;
+  caracteristics: Caracteristics;
+  spaceType:SpaceType
+  name: string;
   // @Prop({ required: true })
   // rentStartAt: Date;
   // @Prop()
---
+++ apps/api/src/infrastructure/repository/schemas/renter.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type RenterDocument = HydratedDocument<Renter>;
+
+@Schema()
+export class Renter {
+  @Prop()
+  lastName: string;
+
+  @Prop()
+  firstName: string;
+
+}
+
+export const RenterSchema = SchemaFactory.createForClass(Renter);
